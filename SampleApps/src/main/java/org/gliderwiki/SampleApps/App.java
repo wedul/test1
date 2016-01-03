3180077cb428a4c284870e181cf34b68aeec0e46
wedul <cjung@pnpsecure.com>
2016-01-03 20:04:33 +0900

    maven 연동 테스트

---
+++ SampleApps/src/main/java/org/gliderwiki/SampleApps/App.java
@@ -0,0 +1,16 @@
+package org.gliderwiki.SampleApps;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    public static int sum(int a, int b){
+    	return a + b;
+    }
+}
